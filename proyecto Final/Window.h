2df26ddc37cd5e8c3a9be93a27df4fa2d57e17f6
Edgar Salazar <edgarslzr33@gmail.com>
2022-11-28 22:08:22 -0600

    Agregar archivos de proyecto.

---
+++ proyecto Final/Window.h
@@ -0,0 +1,49 @@
+#pragma once
+#include<stdio.h>
+#include<glew.h>
+#include<glfw3.h>
+
+class Window
+{
+public:
+	Window();
+	Window(GLint windowWidth, GLint windowHeight);
+	int Initialise();
+	GLfloat getBufferWidth() { return bufferWidth; }
+	GLfloat getBufferHeight() { return bufferHeight; }
+	GLfloat getXChange();
+	GLfloat getYChange();
+	GLfloat getmuevex() { return muevex; }
+	GLfloat getbandera() { return bandera; }
+	GLfloat getmueveHelix() { return helix; }
+	GLfloat getmueveHeliy() { return heliy; }
+	GLfloat getmueveHeliz() { return heliz; }
+	GLint truez() { return ztrue; }
+	bool getShouldClose() {
+		return  glfwWindowShouldClose(mainWindow);}
+	bool* getsKeys() { return keys; }
+	void swapBuffers() { return glfwSwapBuffers(mainWindow); }
+	
+	~Window();
+private: 
+	GLFWwindow *mainWindow;
+	GLint width, height;
+	bool keys[1024];
+	GLint bufferWidth, bufferHeight;
+	void createCallbacks();
+	GLfloat lastX;
+	GLfloat lastY;
+	GLfloat xChange;
+	GLfloat yChange;
+	GLfloat muevex;
+	GLfloat bandera;
+	GLfloat  helix ;
+	GLfloat heliy ;
+	GLfloat heliz ;
+	int ztrue;
+	bool mouseFirstMoved;
+	static void ManejaTeclado(GLFWwindow* window, int key, int code, int action, int mode);
+	static void ManejaMouse(GLFWwindow* window, double xPos, double yPos);
+
+};
+
